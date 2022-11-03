b535338a79f95da3f993dbca299e8204c2150af1
Jorge Precich <precichysim@gmail.com>
2022-11-03 10:24:15 -0600

    fix: javascript guidelines

--- ejercicioJS.js
+++ ejercicioJS.js
@@ -1,20 +1,19 @@
 const num = [8,9,10,11,12,13,14,15,1,2,3,4,5,6,7];
 const numB = [3,2,4,1]
-let orden = (array) => {
-   let lugar = 0;
-   const array_lenght = array.length;
+const wrongNumber = [2,3,5,4,1]
+const correctNumber = [4,1,2,3]
 
-   for (let i = 0; i < array_lenght; i++) {
-
-   if (array[i+1] >= array[i])  {
-
-         return lugar = "giro " +array.indexOf(1) +" veces";
-
-      } else {
-         return -1;
-      }
-   }
-   
+const orden = (array) => {
+  for (let i = 0; i < array.lenght; i++) {
+    if (array[i+1] >= array[i])  {
+      return "giro " +array.indexOf(1) +" veces";
+    } else {
+      return -1;
+    }
+  }
 }
-console.log(orden(num));
-console.log(orden(numB));
+
+//console.log(orden(num));
+//console.log(orden(numB));
+console.log(orden(correctNumber));
+console.log(orden(wrongNumber));
