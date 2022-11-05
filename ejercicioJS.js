61a660ea47a3dc55013ab8afec6673788d6e0e73
chito <chito.pepsi@gmail.com>
2022-11-04 19:13:54 -0600

    a ver si jala la tostadora

--- ejercicioJS.js
+++ ejercicioJS.js
@@ -1,19 +1,21 @@
 const num = [8,9,10,11,12,13,14,15,1,2,3,4,5,6,7];
-const numB = [3,2,4,1]
-const wrongNumber = [2,3,5,4,1]
-const correctNumber = [4,1,2,3]
+const numB = [3,2,4,1];
+const wrongNumber = [2,7,5,2,1];
+const correctNumber = [4,5,6,1,2,3];
+const asend = [2,3,4,5,1];
 
 const orden = (array) => {
-  for (let i = 0; i < array.lenght; i++) {
-    if (array[i+1] >= array[i])  {
-      return "giro " +array.indexOf(1) +" veces";
+  for (const i of array) {
+    if (array[i] < array[i+1]|| array[i] === array.lenght )   {
+      return "giro " + array.indexOf(1) +" veces";
     } else {
       return -1;
-    }
+    } 
   }
 }
-
-//console.log(orden(num));
-//console.log(orden(numB));
-console.log(orden(correctNumber));
 console.log(orden(wrongNumber));
+console.log(orden(correctNumber));
+console.log(orden(asend));
+
+
+
