b8d3eeb586e99f893b779d799ddd0da82212dc70
Jorge Precich <jorge.precich@finalsite.com>
2022-11-04 22:01:00 -0600

    testing bugs

--- ejercicioJS.js
+++ ejercicioJS.js
@@ -1,21 +1,30 @@
 const num = [8,9,10,11,12,13,14,15,1,2,3,4,5,6,7];
 const numB = [3,2,4,1];
-const wrongNumber = [2,7,5,2,1];
+const wrongNumber = [5,6,7,1,2,3,4];
 const correctNumber = [4,5,6,1,2,3];
-const asend = [2,3,4,5,1];
+const asend = [3,4,5,1,2];
 
 const orden = (array) => {
-  for (const i of array) {
+  let variable;
+
+  for (const i of array.keys()) {
+    if (i+1 === array.length) {
+      console.log(1 < undefined)
+      console.log(array[i])
+      console.log(array.length)
+    }
     if (array[i] < array[i+1]|| array[i] === array.lenght )   {
-      return "giro " + array.indexOf(1) +" veces";
+      variable = `giro ${array.indexOf(1)} veces`;
     } else {
-      return -1;
+      variable = -1;
     } 
   }
+
+  return variable;
 }
-console.log(orden(wrongNumber));
-console.log(orden(correctNumber));
-console.log(orden(asend));
 
-
-
+orden(numB);
+//console.log(orden(wrongNumber));
+//console.log(orden(numB));
+//console.log(orden(correctNumber));
+//console.log(orden(asend));
