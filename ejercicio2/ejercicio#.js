9658cb1affbb274eab097fbb80f5d1c5542d6d5c
chito <chito.pepsi@gmail.com>
2022-11-08 00:09:37 -0600

    homework #test ended

--- ejercicio2/ejercicio#.js
+++ ejercicio2/ejercicio#.js
@@ -1,14 +1,25 @@
-// rules #inicial
-//        que le cambie a mayusculas la primera letra
-//        que sea false si tiene mas de 140 chars
-//         si esta vacio return false
+let empty= "";
+let space = "      ";
+let have = "Do We have A Hashtag";
+let test = "test";
+let codewars = "codewars Is Nice";
+let moreThan140 = "Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat";
+let repeat139 = "a".repeat(139);
+let repeat140 = "a".repeat(140);
 
-
-let word = "pepe"
 const hashtag ="#"
-
-function mayuscula (variable) {
-    return word = hashtag + word.charAt(0).toUpperCase() + word.slice(1)
-}
-console.log(mayuscula(word));
-
+function mayuscula (variable){
+    if (variable === "" || variable.trim() === "" || variable.trim().length >= 140) {
+        return false;
+    }   else { 
+        return variable = hashtag + variable.charAt(0).toUpperCase() + variable.slice(1).split(" ").join("");
+        }
+}  
+console.log(mayuscula(empty));
+console.log(mayuscula(space));
+console.log(mayuscula(have));
+console.log(mayuscula(test));
+console.log(mayuscula(codewars));
+console.log(mayuscula(moreThan140));
+console.log(mayuscula(repeat139));
+console.log(mayuscula(repeat140));
