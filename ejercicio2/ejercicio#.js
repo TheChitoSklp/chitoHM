fb3ed79dd91f561cea65746dd1a9eaea8d143882
chito <chito.pepsi@gmail.com>
2022-11-11 00:06:14 -0600

    mini fix variable hashtag

--- ejercicio2/ejercicio#.js
+++ ejercicio2/ejercicio#.js
@@ -1,23 +1,24 @@
 let empty= '                 ola                        ke                                                                                                                           ase';
 let space = "      ";
-let have = "Do We have A Hashtag";
+let have = "Do we have A Hashtag";
 let test = "test";
 let codewars = "codewars Is Nice";
 let moreThan140 = "Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat";
 let repeat139 = "a".repeat(139);
 let repeat140 = "a".repeat(140);
 
-const hashtag ="#"
+
 function mayuscula (variable){
     //.split rompe el array dependiendo de donde se tenga la condicion .filter saca alv los espacios  .map
     let spacesAndCapitalLetter = variable.split(' ')
                                          .filter((deleteSpaces) => deleteSpaces != '')
-                                         .map(capitalLetter => capitalLetter[0].toUpperCase() + capitalLetter.slice(1));
-    let variableSinEspacio = variable.split(" ").join("");
-    if (variableSinEspacio === "" || variableSinEspacio.length >= 140) {
+                                         .map(capitalLetter => capitalLetter[0].toUpperCase() + capitalLetter.slice(1))
+                                         .join('');
+  
+        if (spacesAndCapitalLetter === "" || spacesAndCapitalLetter.length >= 140) {
         return false;
     }   else { 
-        return variableSinEspacio = hashtag + variableSinEspacio.charAt(0).toUpperCase() + variableSinEspacio.slice(1);
+        return spacesAndCapitalLetter = '#' + spacesAndCapitalLetter;
         }
 }  
 console.log(mayuscula(empty));
@@ -29,15 +30,6 @@ console.log(mayuscula(moreThan140));
 console.log(mayuscula(repeat139));
 console.log(mayuscula(repeat140));
 
-
-let variable = 'estoy convirtiendome en mayusculas  tambien si  hay 2 espacios    o mas';
-let palabras = variable.split(' ')
-                       .filter((deleteSpaces) => deleteSpaces != '')
-                       .map(capitalLetter => capitalLetter[0].toUpperCase() + capitalLetter.slice(1))
-                       .toString();
-                       
-
-console.log(palabras);
 
 
 
