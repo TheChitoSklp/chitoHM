3b4408d81f993dac61387b5ec1906a8176d8528c
chito <chito.pepsi@gmail.com>
2022-11-18 08:28:02 -0600

    change squeare class by function square

--- src/index.js
+++ src/index.js
@@ -3,25 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value:null,
-        };
-    }
-
-    render() {
-      return (
-        <button 
-            className="square"
-            onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+function Square(props) {
+  return (
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
   
 class Board extends React.Component {
     constructor(props) {
@@ -32,8 +20,11 @@ class Board extends React.Component {
     }
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X'
-        this.setState({squares:squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState({
+          squares:squares,
+          xIsNext: !this.state.xIsNext,
+        });
     }
     renderSquare(i) {
       return (<Square value={this.state.squares[i]}
